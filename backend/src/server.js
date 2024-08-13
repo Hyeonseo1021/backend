cf4253b453aa9e92cf3161b19e9ad596f28a40dd
Hyeonseo <90162269+Hyeonseo1021@users.noreply.github.com>
2024-08-13 12:35:41 +0900

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,6 +36,10 @@ try {
 app.use(boardRoutes);
 app.use(memberRoutes);
 
+app.get('/', (req, res) => {
+    res.send("hello world");
+});
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(buildPath, 'index.html'));
 });
