bbb71852fb5383cb14b0ab230101fe39e3468006
Hyeonseo <90162269+Hyeonseo1021@users.noreply.github.com>
2024-08-16 19:01:24 +0900

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ const boardRoutes = require('./routes/boardRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({origin: 'https://b-site.netlify.app/' }));
+app.use(cors({origin: true }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
