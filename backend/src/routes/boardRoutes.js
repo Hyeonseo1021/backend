c7a17e830bae915fa9499d1d33bf4dd5e6f338ae
Hyeonseo <90162269+Hyeonseo1021@users.noreply.github.com>
2024-08-26 12:28:15 +0900

    Update boardRoutes.js

--- backend/src/routes/boardRoutes.js
+++ backend/src/routes/boardRoutes.js
@@ -12,11 +12,9 @@ if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
 }
 
-
-
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '..', 'uploads'));
+        cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
