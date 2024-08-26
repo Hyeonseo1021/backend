a1457edabb3e45ff165dee7802eaeb0a4a2336f9
Hyeonseo <90162269+Hyeonseo1021@users.noreply.github.com>
2024-08-26 12:08:04 +0900

    Update boardRoutes.js

--- backend/src/routes/boardRoutes.js
+++ backend/src/routes/boardRoutes.js
@@ -5,6 +5,11 @@ const router = express.Router();
 const postController = require('../controllers/postController');
 
 
+const uploadDir = path.join(__dirname, '..', 'uploads');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, path.join(__dirname, '..', 'uploads'));
